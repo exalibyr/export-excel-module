488740a7b9512404f8890a37d0df40b384081d40
aleksey syromyatnikov <a.syromyatnikov@fleetcor.ru>
2021-06-20 13:15:02 +0300

    v0.2 fix

--- exportXLS.js
+++ exportXLS.js
@@ -1,3 +1,12 @@
+const uriXLS = 'data:application/vnd.ms-excel;base64,';
+const templateXLS = '<html xmlns:o="urn:schemas-microsoft-com:office:office"' +
+    ' xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head>' +
+    '<xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions>' +
+    '<x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml>' +
+    '<meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body>{table}</body></html>';
+
+
+
 const exportData = (sourceComponent, fileName, worksheetName, lang) => {
     let content = buildTable(
         parseForm(sourceComponent, lang),
